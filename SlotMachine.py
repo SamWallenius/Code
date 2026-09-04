b5d0bd0b42c4a5065cd2ffdc9df2534da467a61f
SamWallenius <sam.wallenius@elev.ga.ntig.se>
2026-09-04 10:03:52 +0200

    e

--- SlotMachine.py
+++ SlotMachine.py
@@ -2,7 +2,8 @@ import random
 import tkinter as tk
 import tkinter.font as tfont
 
-
+bet = 1
+budget = 100
 rng1 = 0
 rng2 = 0
 rng3 = 0
@@ -10,15 +11,17 @@ rng3 = 0
 rolling = False
 
 def roll():
-    global rolling
+    global rolling, budget, bet
 
-    if rolling == False:
+    if rolling == False and budget >= bet:
         rolling = True
+        budget -= bet
+        money.configure(text=budget)
         spin(0)
 
 
 def spin(count):
-    global rng1, rng2, rng3
+    global rng1, rng2, rng3, budget, bet
 
     label1.configure(background="white")
     label2.configure(background="white")
@@ -43,43 +46,47 @@ def spin(count):
 
 
 def check():
-    global rolling
+    global rolling, budget, bet
     rolling = False
     if rng1 == rng2 and rng2 == rng3:
         print(f"{rng1} {rng2} {rng3} You win!")
         label1.configure(background="green")
         label2.configure(background="green")
         label3.configure(background="green")
+        budget += bet * 15
 
     elif rng2 == rng1 + 1 and rng3 == rng2 + 1:
         print(f"{rng1} {rng2} {rng3} You win!")
         label1.configure(background="green")
         label2.configure(background="green")
         label3.configure(background="green")
+        budget += bet * 5
 
     elif rng1 == rng2 and not rng2 == rng3:
-        print(f"{rng1} {rng2} {rng3} You lost.")
+        print(f"{rng1} {rng2} {rng3} You got your money back.")
         label1.configure(background="yellow")
         label2.configure(background="yellow")
         label3.configure(background="yellow")
+        budget += bet
 
     elif rng2 == rng3 and not rng1 == rng2:
-        print(f"{rng1} {rng2} {rng3} You lost.")
+        print(f"{rng1} {rng2} {rng3} You got your money back.")
         label1.configure(background="yellow")
         label2.configure(background="yellow")
         label3.configure(background="yellow")
+        budget += bet
 
     else:
         print(f"{rng1} {rng2} {rng3} You lose.")
         label1.configure(background="red")
         label2.configure(background="red")
         label3.configure(background="red")
-
+    money.configure(text=budget)
 
 root = tk.Tk()
 root.title("Slot Machine")
 root.configure(background="black")
-root.geometry("300x150")
+root.geometry("850x150")
 
 textfont = tfont.Font(family="Arial", size=80)
 
@@ -94,12 +101,20 @@ label3.pack(side=tk.LEFT, pady=20)
 
 button = tk.Button(
     root,
-    text="ROLL",
+    text="ROLL (1$)",
     command=roll,
     height=6,
     width=12
 )
 button.place(relx=1.0, rely=1.0, anchor="se")
 button.pack(side=tk.LEFT, pady=20)
+
+money = tk.Label(root, text="100", font=textfont, bg="black", fg="green")
+money.pack(side=tk.LEFT, pady=20)
+moneytext = tk.Label(root, text="$", font=textfont, bg="black", fg="green")
+moneytext.pack(side=tk.LEFT, pady=20)
+
+bettext = tk.Label(root,font=textfont,text="Bet: 1")
+bettext.pack(side=tk.RIGHT, pady=20)
 
 root.mainloop()
