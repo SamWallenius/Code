254b1e48aa7c6b33dd6bbb7227d6735651f82702
SamWallenius <sam.wallenius@elev.ga.ntig.se>
2026-09-17 14:20:58 +0200

    wawaworld

---
+++ Uppgifter/Uppgift2.py
@@ -0,0 +1,8 @@
+import random
+t1 = random.randint(1,6)
+t2 = random.randint(1,6)
+summa = t1 + t2
+
+print("Tärning 1: " + str(t1))
+print("Tärning 2: " + str(t2))
+print("Summa: " + str(summa))
---
+++ Uppgifter/Uppgift3.py
@@ -0,0 +1,8 @@
+password = "python"
+
+fråga = input("Vad är lösenordet?: ")
+
+if fråga == password: 
+    print("Välkommen!")
+else:
+    print("Fel lösenord!")
